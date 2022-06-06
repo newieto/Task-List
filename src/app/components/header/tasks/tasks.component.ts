a11f4d3e801735376a5c75127aa4ec204b74931c
newieto <39123762+newieto@users.noreply.github.com>
2022-06-06 17:16:01 -0300

    Add files via upload

---
+++ src/app/components/header/tasks/tasks.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import {Task} from '../../Task';
+
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit(): void {
+    //Like Promise
+    this.taskService.getTasks().subscribe((tasks)=>[
+      this.tasks = tasks
+    ]);
+  }
+
+}
